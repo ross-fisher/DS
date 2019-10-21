d30138f87146e65586ab09291343864b71b01403
KyleHaggin <KyleHaggin@users.noreply.github.com>
2019-10-21 10:57:54 -0700

    User data now hidden

--- prawapi.py
+++ prawapi.py
@@ -1,12 +1,24 @@
 # import important libraries
 import praw
+import configparser
 
+# grab userdata from hidden files
+config = configparser.ConfigParser()
+config.read('secrets.ini')
+user_agent = config.get('reddit', 'user_agent')
+client_id = config.get('reddit', 'client_id')
+client_secret = config.get('reddit', 'client_secret')
+password = config.get('reddit', 'password')
+username = config.get('reddit', 'username')
+
+# get api access token
 reddit = praw.Reddit(
-    client_id='fpc5FKLaEpjoyw',
-    client_secret='h1EhwL6_wf2AS6RxMT6_Gt1ofY4',
-    user_agent='BWPostHere/0.1 by u/BWPostHere',
-    username='BWPostHere',
-    password='Passlock1'
+    client_id=client_id,
+    client_secret=client_secret,
+    user_agent=user_agent,
+    username=username,
+    password=password
 )
 
+# test
 print(reddit.read_only)
