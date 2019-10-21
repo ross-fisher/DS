b0724905bce39a898cb8662b627a0d20082a590a
KyleHaggin <KyleHaggin@users.noreply.github.com>
2019-10-21 13:59:25 -0700

    Add function to get subreddit info

--- src/prawapi.py
+++ src/prawapi.py
@@ -21,6 +21,7 @@ reddit = praw.Reddit(
     password=password
 )
 
+<<<<<<< Updated upstream
 # for updating the sql database
 def get_top_subreddits_table(reddit, n=50):
     popular_subreddits = reddit.subreddits.popular()[0:n]
@@ -31,20 +32,74 @@ def get_top_subreddits_table(reddit, n=50):
             name=sr['display_name']
         )]
     df = pd.DataFrame(subreddit_infos)
+=======
+# test
+print(reddit.read_only)
+
+# test for posts in a subreddit
+# for submission in reddit.subreddit('redditdev').hot(limit=10):
+#     print(submission.title)
+
+subreddit = reddit.subreddit('subreddits')
+
+# test for top subreddits
+# for submission in subreddit.top(limit=20):
+#     print(submission.title)
+#     print(submission.score)
+#     print(submission.id)
+#     print(submission.url)
+
+# find the popular subreddits, currently top 5
+top_subreddits = list(reddit.subreddits.popular())[0:5]
+top_subreddits = [s.display_name for s in top_subreddits]
+print(top_subreddits)
+
+# print(reddit.subreddit('redditdev').description)
+# print(reddit.subreddit('redditdev').subscribers)
+>>>>>>> Stashed changes
+
+
+def subreddit_info(subreddits):
+    rows = []
+    for x in range(len(top_subreddits)):
+        rpath = reddit.subreddit((top_subreddits[x]))
+        rows.append(
+            dict(
+                subreddit_name=top_subreddits[x],
+                subreddit_description=rpath.description,
+                subreddit_id=rpath.id,
+                subreddit_nsfw=rpath.over18,
+                subreddit_subscribers=rpath.subscribers
+            )
+        )
+    subreddit_info = pd.DataFrame(rows)
+    return subreddit_info
+
+
+print(subreddit_info(top_subreddits))
+
 
 def sample_comments(sr, n=20):
     rows = []
+<<<<<<< Updated upstream
     comments = list(sr.comments(limit=n)) # TODO check if this sample is fair
+=======
+    comments = list(sr.comments())[0:n]  # TODO check if this sample is fair
+>>>>>>> Stashed changes
     for comment in comments:
         rows.append(
             dict(body=comment.body,
-                body_html=comment.body_html,
-                id=comment.id,
-                author_id=comment.author.id,
-                subreddit_id=sr.id,
-                created_utc=comment.created_utc))
+                 body_html=comment.body_html,
+                 id=comment.id,
+                 author_id=comment.author.id,
+                 subreddit_id=sr.id,
+                 created_utc=comment.created_utc))
     df = pd.DataFrame(rows)
     return df 
 
+<<<<<<< Updated upstream
 subreddit = reddit.subreddit('learnpython')
+=======
+subreddit = reddit.subreddit('python')
+>>>>>>> Stashed changes
 print(sample_comments(subreddit))
