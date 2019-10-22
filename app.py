57aaf97f9b53d4e6db9bb325641e60491eb6fc8a
KyleHaggin <KyleHaggin@users.noreply.github.com>
2019-10-22 14:11:31 -0700

    deleted unneeded/template code. conforms to pep8 standards

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from decouple import config
 from scipy.sparse import bsr_matrix
 from joblib import load
+import praw
+import configparser
+import pandas as pd
 
 # config('DATABASE_URL')
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///the_db.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 DB = SQLAlchemy(app)
-# temp
+
+
 class Comment(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     subbredditKey = DB.Column(
@@ -30,12 +34,9 @@ class Subreddit(DB.Model):
     nsfw = DB.Column(DB.Boolean)
     subscribers = DB.Column(DB.Integer)
 
+
 DB.drop_all()
 DB.create_all()
-
-import praw
-import configparser
-import pandas as pd
 
 # grab userdata from hidden files
 config = configparser.ConfigParser()
@@ -55,65 +56,71 @@ reddit = praw.Reddit(
     password=password
 )
 
+
 def update_subreddit_table(subreddits):
     try:  # try doesn't actually work
         for subreddit in subreddits:
-            print('THNHAOETUHNSAOEUHTNSAOEUHTNH')
             sr = Subreddit(
-                name=subreddit.display_name, 
+                name=subreddit.display_name,
                 description=subreddit.description,
-                subredditID = subreddit.id,
-                nsfw = subreddit.over18,
-                subscribers = subreddit.subscribers
+                subredditID=subreddit.id,
+                nsfw=subreddit.over18,
+                subscribers=subreddit.subscribers
             )
-            print("display name", subreddit.display_name)
-            print("descritpion", subreddit.description)
-            print("id", subreddit.id)
-            print("over18", subreddit.over18)
-            print("subscribers", subreddit.subscribers)
-            #print(Subreddit.query.filter_by(subredditID=subreddit.id))
+
+            # Print functions for test purposes, uncomment to enable
+            # print("display name", subreddit.display_name)
+            # print("descritpion", subreddit.description)
+            # print("id", subreddit.id)
+            # print("over18", subreddit.over18)
+            # print("subscribers", subreddit.subscribers)
+            # print(Subreddit.query.filter_by(subredditID=subreddit.id))
             DB.session.add(sr)
         print("hello")
         DB.session.commit()
         print("hello2")
-        #print(Subreddit.query.all())
+        # print(Subreddit.query.all())
         return 'okay'
     except Exception as e:
         return f'Error {e}'
     return 'Okay'
 
+
 def update_user_table(users):
     pass
 
+
 def update_comments_table(comments):
-    try: 
+    try:
         for comment in comments:
             c = Comment(body=comment.body,
-                comment_id=comment.id,
-                author_id=comment.author.id,
-                subreddit_id=comment.subreddit_id,
-                created_utc=comment.created_utc)
+                        comment_id=comment.id,
+                        author_id=comment.author.id,
+                        subreddit_id=comment.subreddit_id,
+                        created_utc=comment.created_utc)
             DB.session.add(c)
         DB.session.commit()
     except Exception as e:
         print(f'Error {e}')
 
- def update_submissions_table(submission):
-    try: 
+
+def update_submissions_table(submission):
+    try:
         for submission in submission:
             c = Comment(name=submission.name,
-                submission_id=submission.id,
-                author_id=submission.author.id,
-                body=submission.selftext or "",
-                url=submission.url,
-                num_comments=submission.num_comments,
-                score=submission.score,
-                subreddit_name=submission.subreddit,
-                created_utc=submission.created_utc)
+                        submission_id=submission.id,
+                        author_id=submission.author.id,
+                        body=submission.selftext or "",
+                        url=submission.url,
+                        num_comments=submission.num_comments,
+                        score=submission.score,
+                        subreddit_name=submission.subreddit,
+                        created_utc=submission.created_utc)
             DB.session.add(c)
         DB.session.commit()
     except Exception as e:
-        print(f'Error {e}')   
+        print(f'Error {e}')
+
 
 @app.route('/update_tables')
 def update_all_tables():
@@ -122,10 +129,12 @@ def update_all_tables():
     update_subreddit_table(top_subreddits)
     return 'okay'
 
+
 # find the top x subreddits, defualt 100
 def get_top_subreddits(n=100):
     top_subs = list(reddit.subreddits.popular())[0:n]
     return top_subs
+
 
 def test():
     top_subreddits = get_top_subreddits(n=5)
@@ -135,45 +144,3 @@ def test():
 @app.route('/')
 def root():
     return 'Hello'
-
-
-# API route Comments
-@app.route('/api/comments', methods=['POST'])
-def commentsAPI():
-    pass  # API here
-
-
-@app.route('/api/subreddits', methods=['POST'])
-def subredditsAPI():
-    pass  # API here
-
-
-
-# # class Template
-# class Book(DB.Model):
-#     id = DB.Column(DB.Integer, primary_key=True, autoincrement=True)
-#     webpage = DB.Column(DB.BigInteger)
-#     title = DB.Column(DB.String(300))
-#     author = DB.Column(DB.String(100))
-#     descrip = DB.Column(DB.String(25000))
-#     rating = DB.Column(DB.Float)
-#     num_ratings = DB.Column(DB.String(30))
-#     num_reviews = DB.Column(DB.String(30))
-#     isbn = DB.Column(DB.String(110))
-#     isbn13 = DB.Column(DB.String(110))
-#     binding = DB.Column(DB.String(100))
-#     edition = DB.Column(DB.String(125))
-#     num_pages = DB.Column(DB.String(100))
-#     published_on = DB.Column(DB.String(150))
-#     genres = DB.Column(DB.String(300))
-
-#     def __repr__(self):
-#         return f'Book: {self.title} writtien by {self.author}'
-
-
-# # API route template
-# @app.route('/api/description', methods=['POST'])
-# def api():
-#     description = request.get_json('description')['description']
-#     output = get_books(description)
-#     return jsonify(output)
