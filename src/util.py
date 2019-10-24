2b3180e1cd876bae97cb97fbd3b60801c76092cf
KyleHaggin <KyleHaggin@users.noreply.github.com>
2019-10-23 17:40:25 -0700

    Conform to pep8

--- src/util.py
+++ src/util.py
@@ -1,7 +1,8 @@
 from gensim.utils import simple_preprocess
 from gensim.parsing.preprocessing import STOPWORDS
 
+
 def tokenize(doc):
-   return [token for token in simple_preprocess(doc) if token not in STOPWORDS]
-
-
+    return [
+       token for token in simple_preprocess(doc) if token not in STOPWORDS
+       ]
